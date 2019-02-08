29953eff570214f14bf922754288c8ab95577d44
Gordon Freeman <frostyslion@gmail.com>
2019-02-08 11:24:36 +0200

    top done

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: [
+    './base.signup.component.css',
+    './desktop.signup.component.css',     //  device specific
+    './mobile.signup.component.css',      //  device specific
+    './top-case.signup.component.css',    //  case specific
+    './bottom-case.signup.component.css'   //  case specific
+  ]
 })
 export class SignupComponent implements OnInit {
+
+  @Input('is-top') isTop: boolean;
+  @Input('is-bottom') isBottom: boolean;
 
   constructor() { }
 
