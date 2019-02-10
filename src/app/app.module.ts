2ff582df1c1c229b4234e8058d0bcbfd4e49f7a4
Gordon Freeman <frostyslion@gmail.com>
2019-02-10 12:42:36 +0200

    security and preface sections done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SecurityComponent } from './components/security/security.component';
+import { DlaTeamComponent } from './components/dla-team/dla-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SignupComponent,
-    TopBarComponent
+    TopBarComponent,
+    SecurityComponent,
+    DlaTeamComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/dla-team/dla-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dla-team',
+  templateUrl: './dla-team.component.html',
+  styleUrls: ['./dla-team.component.css']
+})
+export class DlaTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
