04203bd71ff9f6e77347210c43a500665753fca7
Gordon Freeman <frostyslion@gmail.com>
2019-02-16 13:21:37 +0200

    presignup page responsive

--- src/app/components/pre-signup-page/pre-signup-page.component.ts
+++ src/app/components/pre-signup-page/pre-signup-page.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pre-signup-page',
   templateUrl: './pre-signup-page.component.html',
-  styleUrls: ['../signup-page/signup-page.component.css','./pre-signup-page.component.css']
+  styleUrls: [
+    '../signup-page/signup-page.component.css',
+    './pre-signup-page.component.css',
+    './pre-signup-page-mw1100px.component.css',
+  ]
 })
 export class PreSignupPageComponent implements OnInit {
 
--- src/app/components/pre-signup/pre-signup.component.ts
+++ src/app/components/pre-signup/pre-signup.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pre-signup',
   templateUrl: './pre-signup.component.html',
-  styleUrls: ['./pre-signup.component.css']
+  styleUrls: [
+    './pre-signup.component.css',
+    './pre-signup-mw550px.component.css'
+  ]
 })
 export class PreSignupComponent implements OnInit {
 
